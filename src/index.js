911b8d7c29c53099fb2e690fd47d35696f336e6c
VICOrepo <viral.jain.2019@ecajmer.ac.in>
2023-04-13 12:24:09 +0530

    formikyup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 
 import "./App.css";
 import App from "./App";
+import SignupForm from "./Component/Basic";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SignupForm />
   </React.StrictMode>,
   rootElement
 );
